6010c33cced7941545c94fff815b093fb530ee96
Jbasilio55 <j.l.basilio55@gmail.com>
2023-05-24 11:40:44 -0400

    About me added

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,16 +4,16 @@ import Main from "./components/Main";
 import Work from "./components/Work";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import Education from "./components/Education";
+import AboutMe from "./components/AboutMe";
 
 function App() {
   return (
     <div>
       <Sidenav />
       <Main />
+      <AboutMe />
       <Work />
       <Projects />
-      <Education />
       <Contact />
     </div>
   );
--- portfolio/src/components/AboutMe.jsx
+++ portfolio/src/components/AboutMe.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const AboutMe = () => {
-  return <div>AboutMe</div>;
+  return (
+    <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+      <h1 className="text-4xl font-bold text-center text-[#001b5e]">
+        About Me
+      </h1>
+      <p className="text-center py-8 text-xl text-stone-500">
+        Aspiring software engineer with a strong foundation in Java, Ruby on
+        Rails, and JavaScript-based programming. My approach to problem-solving
+        is both creative and meticulous, and I thrive in team-based settings,
+        consistently offering reliability, adaptability, and an insatiable
+        curiosity to learn and innovate. My diverse background in security
+        management and law enforcement has furnished me with significant
+        leadership acumen, an exceptional eye for detail, and the capacity to
+        communicate effectively and resolve issues within high-pressure
+        contexts. I am currently on the cusp of earning a Bachelor of Science
+        degree in Computer Science and have fortified my technical arsenal
+        through attending the renowned Flatiron coding bootcamp and an intensive
+        Java Application Development course sponsored by Infosys. As I continue
+        to evolve professionally, I remain deeply committed to harnessing my
+        skills and experiences to craft impactful and high-performing software
+        solutions that meet and exceed expectations.
+      </p>
+    </div>
+  );
 };
 
 export default AboutMe;
