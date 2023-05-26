dbd361f41cc8e640062604f7cb773cf722c9b552
Jbasilio55 <j.l.basilio55@gmail.com>
2023-05-26 19:46:18 -0400

    Added the about me to sidenav

--- portfolio/src/components/AboutMe.jsx
+++ portfolio/src/components/AboutMe.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AboutMe = () => {
   return (
-    <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+    <div id="aboutMe" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
       <h1 className="text-4xl font-bold text-center text-[#001b5e]">
         About Me
       </h1>
--- portfolio/src/components/Sidenav.jsx
+++ portfolio/src/components/Sidenav.jsx
@@ -4,6 +4,7 @@ import {
   AiOutlineHome,
   AiOutlineProject,
   AiOutlineMail,
+  AiOutlineLaptop,
 } from "react-icons/ai";
 import { BsPerson } from "react-icons/bs";
 import { GrProjects } from "react-icons/gr";
@@ -33,7 +34,15 @@ const Sidenav = () => {
           </a>
           <a
             onClick={handleNav}
-            href="#work"
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <AiOutlineLaptop size={20} />
+            <span className="pl-4">About Me</span>
+          </a>
+          <a
+            onClick={handleNav}
+            href="#aboutMe"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <GrProjects size={20} />
@@ -75,6 +84,12 @@ const Sidenav = () => {
             className="rounded-full shadow-lg bg-gray-100 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
             <AiOutlineHome size={20} />
+          </a>
+          <a
+            href="#aboutMe"
+            className="rounded-full shadow-lg bg-gray-100 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
+          >
+            <AiOutlineLaptop size={20} />
           </a>
           <a
             href="#work"
