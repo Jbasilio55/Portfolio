a28c959486a18a3d210547dcaf174b727ebe9996
Jbasilio55 <54733957+Jbasilio55@users.noreply.github.com>
2025-07-10 13:44:21 -0400

    Update Projects.jsx
    change max width of title for projects

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -8,7 +8,7 @@ import project5 from "../img/converter.jpg";
 
 const Projects = () => {
   return (
-    <div id="projects" className="max-w-[1040] m-auto md:pl-20 p-4 py-16">
+    <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
       <h1 className="text-4xl font-bold text-center text-[#001b5e]">
         Projects
       </h1>
