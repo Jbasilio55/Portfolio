19caa35a82194454f7a29277fa3778ec902d869a
Jbasilio55 <54733957+Jbasilio55@users.noreply.github.com>
2025-07-10 13:36:39 -0400

    Update AboutMe.jsx
    made text position to left and added a few more brakes in paragraphs

--- portfolio/src/components/AboutMe.jsx
+++ portfolio/src/components/AboutMe.jsx
@@ -6,16 +6,18 @@ const AboutMe = () => {
       <h1 className="text-4xl font-bold text-center text-[#001b5e]">
         About Me
       </h1>
-      <p className="text-center py-8 text-xl text-stone-500">
+      <p className="text-left py-8 text-xl text-stone-500">
         I’m a Software Engineer with a background in full-stack development, cloud-based applications, 
         and machine learning. After earning my B.S. in Computer Science from Western Governors University 
         and completing immersive training programs at Flatiron School and Infosys, I’ve built projects that 
         showcase both front-end design and back-end logic—from e-commerce stores with Google Authentication to 
         machine learning models predicting housing prices.
         <br/>
+        <br/>
         Before tech, I led teams in high-pressure environments as a Lead Security Officer at a Level One Trauma Center. 
         That experience sharpened my communication, problem-solving, and leadership skills—traits I now bring to 
         collaborative engineering teams.
+        <br/>
         <br/>
         I’m passionate about building clean, user-focused solutions and continuously learning new technologies. 
         I’m currently looking for opportunities where I can contribute to real-world software development 
