ea7eaa20ca1287e24bfa04a2fba32f0035a7cddd
Jbasilio55 <54733957+Jbasilio55@users.noreply.github.com>
2025-08-11 12:17:36 -0400

    Update AboutMe.jsx
    changes to about me

--- portfolio/src/components/AboutMe.jsx
+++ portfolio/src/components/AboutMe.jsx
@@ -8,19 +8,19 @@ const AboutMe = () => {
       </h1>
       <p className="text-left py-8 text-xl text-stone-500">
         I’m a Software Engineer with a background in full-stack development, cloud-based applications, 
-        and machine learning. After earning my B.S. in Computer Science from Western Governors University 
-        and completing immersive training programs at Flatiron School and Infosys, I’ve built projects that 
-        showcase both front-end design and back-end.
+        and machine learning. After earning my B.S. in Computer Science from Western Governors University and 
+        completing immersive training programs at Flatiron School and Infosys, I’ve built projects showcasing front-end 
+        design and back-end development.
         <br/>
         <br/>
-        Before tech, I was an Auxiliary Police Officer and led teams in high-pressure environments as a Lead Security 
-        Officer at a Level One Trauma Center. That experience sharpened my communication, problem-solving, and leadership 
-        skills—traits I now bring to collaborative engineering teams.
+        Before tech, I was an Auxiliary Police Officer and led teams in high-pressure environments as a 
+        Lead Security Officer at a Level One Trauma Center. That experience sharpened my communication, 
+        problem-solving, and leadership skills—traits I now bring to collaborative engineering teams.
         <br/>
         <br/>
         I’m passionate about building clean, user-focused solutions and continuously learning new technologies. 
-        I’m currently looking for opportunities where I can contribute to real-world software development 
-        and grow under strong technical mentorship.
+        I’m looking for opportunities to contribute to real-world software development and grow under strong 
+        technical mentorship.
       </p>
     </div>
   );
